d416fdbb208de1f56009fac8ba041858c040848c
cedricnator <c.kirmayr01@ufromail.cl>
2024-09-24 20:13:10 -0300

    Refactor .gitignore and main-servidor.py

--- src/app/servidor/main-servidor.py
+++ src/app/servidor/main-servidor.py
@@ -6,58 +6,56 @@ import signal
 clients = []
 
 def broadcast(message, sender_socket):
-    """Send the message to all clients except the sender."""
-    for client in clients:
-        if client != sender_socket:
-            try:
-                client.send(message)
-            except socket.error:  
-                # If sending fails, remove the client
-                client.close()
-                clients.remove(client)
+   """Send the message to all clients except the sender."""
+   for client in clients:
+      if client != sender_socket:
+         try:
+               client.send(message)
+         except socket.error:  
+               # If sending fails, remove the client
+               client.close()
+               clients.remove(client)
 
 def handle_client(client_socket):
-    """Handle incoming messages from a client."""
-    while True:
-        try:
-            message = client_socket.recv(1024)
-            if message:
-                print(f"{message.decode('utf-8')}")
-                broadcast(message, client_socket)
-            else:
-                break
-        except socket.error:
-            break
+   """Handle incoming messages from a client."""
+   while True:
+      try:
+         message = client_socket.recv(1024)
+         if message:
+               print(f"{message.decode('utf-8')}")
+               broadcast(message, client_socket)
+         else:
+               break
+      except socket.error:
+         break
 
-    # Remove the client if connection is lost
-    client_socket.close()
-    clients.remove(client_socket)
+   # Remove the client if connection is lost
+   client_socket.close()
+   clients.remove(client_socket)
 
 def main():
-    try:
-        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server.bind(("0.0.0.0", 10000))
-        server.listen(5)
-        print("Servidor inicializado, esperando conexión...")
-        def sigint_handler():
-            print("handler")
-            for client in clients:
-                client.close()
-            server.close()
-        signal.signal(signal.SIGINT,sigint_handler)
-        
-            
-        
-        while True:
-            client_socket, addr = server.accept()
-            print(f"Conexión exitosa con: {addr}")
-            clients.append(client_socket)
-            thread = threading.Thread(target=handle_client, args=(client_socket,))
-            thread.start()
+   try:
+      server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+      server.bind(("0.0.0.0", 10000))
+      server.listen(5)
+      print("Servidor inicializado, esperando conexión...")
+      def sigint_handler():
+         print("handler")
+         for client in clients:
+               client.close()
+         server.close()
+      signal.signal(signal.SIGINT,sigint_handler)
+      
+      while True:
+         client_socket, addr = server.accept()
+         print(f"Conexión exitosa con: {addr}")
+         clients.append(client_socket)
+         thread = threading.Thread(target=handle_client, args=(client_socket,))
+         thread.start()
 
-    finally:
-        # Clean up the connection
-        server.close()
+   finally:
+      # Clean up the connection
+      server.close()
 
 
 if __name__ == "__main__":
